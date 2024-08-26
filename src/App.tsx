0b21c698f67185ff9671692f71b31830adecb6ee
MielAndMoon <dmestasa@gmail.com>
2024-08-25 23:02:16 -0500

    feat(app): implement useEffect to get transactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react'
+import { fetchTransactions } from './api/api'
+import { TransactionList } from './components/TransactionList'
 import { RootLayout } from './components/layout'
+import type { TransactionModel } from './types'
 
 function App() {
+  const [transactions, setTransactions] = useState<Array<TransactionModel>>([])
+
+  useEffect(() => {
+    const getTransactions = async () => {
+      try {
+        const data = await fetchTransactions(
+          'http://localhost:8000/transactions',
+        )
+        setTransactions(data)
+      } catch (error) {
+        console.error('Failed to fetch', error)
+      }
+    }
+
+    getTransactions()
+  }, [])
+
   return (
     <RootLayout>
-
+      <TransactionList transactions={transactions} />
     </RootLayout>
   )
 }

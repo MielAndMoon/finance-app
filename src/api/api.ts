2ab3f853bc77005adf9c58efcd6aa886852e8fed
MielAndMoon <dmestasa@gmail.com>
2024-08-29 17:17:24 -0500

    feat(api): add insert transaction method

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,38 @@
+import type { Transaction, TransactionModel } from '@/types'
+
 export const fetchTransactions = async (url: string) => {
   try {
     const response = await fetch(url)
 
     if (!response.ok) throw new Error('Error getting data')
 
-    const transactions = await response.json()
+    const transactions: Array<TransactionModel> = await response.json()
     return transactions
   } catch (error) {
     console.error('Error feching data: ', error)
+    throw error
+  }
+}
+
+export const insertTransaction = async (
+  url: string,
+  transaction: Transaction,
+) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(transaction),
+    })
+
+    if (!response) throw new Error('Error insert transaction')
+
+    const newTransaction: TransactionModel = await response.json()
+    return newTransaction
+  } catch (error) {
+    console.error('Error insert new transaction: ', error)
     throw error
   }
 }

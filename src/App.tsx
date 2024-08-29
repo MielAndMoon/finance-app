8b1bc69e490c1227e32612699a1b7575e3460c97
MielAndMoon <dmestasa@gmail.com>
2024-08-29 17:50:13 -0500

    feat(app): add the transaction form and their handler methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from 'react'
+import { type ChangeEvent, useEffect, useState } from 'react'
 import { fetchTransactions } from './api/api'
 import { TransactionList } from './components/TransactionList'
 import { RootLayout } from './components/layout'
 import type { Transaction, TransactionModel } from './types'
+import { FormTransaction } from './components/FormTransaction'
+import { Checkbox } from '@/components/ui/checkbox'
+import { InputControl } from './components/InputControl'
+import { Navbar } from './components/Navbar'
 
 function App() {
   const [transactions, setTransactions] = useState<Array<TransactionModel>>([])
@@ -29,9 +33,67 @@ function App() {
     getTransactions()
   }, [])
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormTransaction({
+      ...formTransaction,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleAmount = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormTransaction({
+      ...formTransaction,
+      amount: e.target.valueAsNumber,
+    })
+  }
+
+  const handleChecked = (checked: boolean) => {
+    setFormTransaction({
+      ...formTransaction,
+      is_income: checked,
+    })
+  }
+
   return (
     <RootLayout>
-      <TransactionList transactions={transactions} />
+      <div className="grid px-12">
+        <Navbar />
+        <section className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+          <FormTransaction onSubmit={}>
+            <InputControl
+              label="description"
+              onChange={handleChange}
+              value={formTransaction.description}
+            />
+            <InputControl
+              label="category"
+              onChange={handleChange}
+              value={formTransaction.category}
+            />
+            <InputControl
+              label="date"
+              onChange={handleChange}
+              value={formTransaction.date}
+            />
+            <InputControl
+              label="amount"
+              onChange={handleAmount}
+              value={formTransaction.amount.toString()}
+              type="number"
+            />
+            <div className="flex flex-row items-center gap-4">
+              <Checkbox
+                id="income"
+                name="income"
+                checked={formTransaction.is_income}
+                onCheckedChange={handleChecked}
+              />
+              <label htmlFor="income">Is income?</label>
+            </div>
+          </FormTransaction>
+          <TransactionList transactions={transactions} />
+        </section>
+      </div>
     </RootLayout>
   )
 }

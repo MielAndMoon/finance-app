7e5256f9d663fa2eb4a9b0b8d45ee7f8c15043ac
MielAndMoon <dmestasa@gmail.com>
2024-08-29 16:07:49 -0500

    feat(components): update transaction form ui and logic

--- src/components/FormTransaction.tsx
+++ src/components/FormTransaction.tsx
@@ -1,46 +1,30 @@
-import { Card, CardContent, CardHeader, CardTitle, CardFooter } from './ui/card'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
-import { Checkbox } from '@/components/ui/checkbox'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import type { FormEvent } from 'react'
 import { Button } from '@/components/ui/button'
 
 interface Props {
-  child: React.ReactNode
+  children: React.ReactNode
+  onSubmit: (e: FormEvent<HTMLFormElement>) => void
 }
 
-export const FormTransaction = () => {
+export const FormTransaction = ({ children, onSubmit }: Props) => {
   return (
-    <Card className="border-none lg:max-w-2xl">
-      <CardHeader>
-        <CardTitle className="text-lg">Add new transaction</CardTitle>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-4">
-          <Label htmlFor="description">Description</Label>
-          <Input id="description" />
-        </div>
-        <div className="grid gap-4">
-          <Label htmlFor="category">Category</Label>
-          <Input id="category" />
-        </div>
-        <div className="grid gap-4">
-          <Label htmlFor="date">Date</Label>
-          <Input id="date" />
-        </div>
-        <div className="grid gap-4">
-          <Label htmlFor="amount">Amount</Label>
-          <Input id="amount" />
-        </div>
-        <div className="flex flex-row items-center gap-4">
-          <Checkbox id="income" />
-          <label htmlFor="income">
-            Is income?
-          </label>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className='w-full'>Save</Button>
-      </CardFooter>
-    </Card>
+    <form onSubmit={onSubmit}>
+      <Card className="border-none lg:max-w-2xl">
+        <CardHeader>
+          <CardTitle className="text-lg">Add new transaction</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4">{children}</CardContent>
+        <CardFooter>
+          <Button className="w-full">Save</Button>
+        </CardFooter>
+      </Card>
+    </form>
   )
 }
